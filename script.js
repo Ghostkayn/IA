86e55f150b53aa12ac58b3a1b282ccaa13b02556
Ghostkayn <126803461+Ghostkayn@users.noreply.github.com>
2024-05-29 13:57:41 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaPrincipal = documente.querySelector(".caixa-principal");
+const caixaPerguntas = documente.querySelector(".caixa-perguntas");
+const caixaAlternativa = documente.querySelector(".caixa-alternativa");
+const caixaResultado = documente.querySelector(".caixa-resultdo");
+const textoReultado = ocumente.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+   ];`
+   const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
